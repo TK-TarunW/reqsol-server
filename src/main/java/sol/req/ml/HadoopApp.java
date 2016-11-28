5d81fa8994df6e19a76f02fae40cf0c09539eede
TK-tarunw <tk.tarunw@gmail.com>
2016-11-28 19:11:48 +0530

    hadoop

--- src/main/java/sol/req/ml/HadoopApp.java
+++ src/main/java/sol/req/ml/HadoopApp.java
@@ -1,7 +1,51 @@
 package sol.req.ml;
 
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
 /**
  * Created by tarun.walia on 11/28/2016.
  */
-public class HadoopApp {
+public class HadoopApp extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new HadoopApp(), args);
+        System.exit(exitCode);
+    }
+
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage: %s [generic options] <input> <output>\n",
+                    getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Job job = Job.getInstance();
+        job.setJarByClass(HadoopApp.class);
+        job.setJobName("WordCounter");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        int returnValue = job.waitForCompletion(true) ? 0 : 1;
+        System.out.println("job.isSuccessful " + job.isSuccessful());
+        return returnValue;
+    }
 }
+
